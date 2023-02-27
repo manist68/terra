e78ce04b5e339e85a3e647f4913e2f3f9a307763
manist68 <120625434+manist68@users.noreply.github.com>
2023-02-27 13:39:57 +0530

    Add files via upload

---
+++ cloud/rg/main.tf
@@ -0,0 +1,57 @@
+variable "prefix" {
+  default = "MStesting"
+}
+variable "location"{
+  default = "West Europe"
+}
+
+provider "azurerm" {
+  skip_provider_registration = "true"
+  #version = "=2.15"
+  features {}
+}
+terraform { 
+  required_providers { 
+    azurerm = { 
+      source  = "hashicorp/azurerm" 
+      version = "=2.46.0" 
+    } 
+  } 
+    backend "azurerm" {   
+        resource_group_name  = "nt-poc-akshaya"
+        storage_account_name = "sinkstrgadf" 
+        container_name       = "terra" 
+        key                  = "rg/terraform.tfstate" 
+    } 
+} 
+
+
+resource "azurerm_resource_group" "MStest" {
+  name     = "${var.prefix}-resources"
+  location = "${var.location}"
+
+  tags = {
+    name        = "mani"
+    environment = "testing"
+  }
+}
+
+resource "azurerm_storage_account" "MStest" {
+  name                     = "storageaccountmstest3"
+  resource_group_name      = azurerm_resource_group.MStest.name
+  location                 = azurerm_resource_group.MStest.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.MStest.name
+}
+output "resource_group_region" {
+  value = azurerm_resource_group.MStest.location
+}
+
