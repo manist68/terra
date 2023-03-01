e6e64f70a0c34211e317e95777f7c7536f2fee5b
manist68 <120625434+manist68@users.noreply.github.com>
2023-03-01 16:13:02 +0530

    Update mountdisk.sh

--- vm/mountdisk.sh
+++ vm/mountdisk.sh
@@ -12,45 +12,77 @@ sudo echo UUID=\"`(blkid /dev/sd${drive}1 -s UUID -o value)`\" /datadrive       
 sudo chown azureuser:azureuser /datadrive
 
 
+File Edit Options Buffers Tools Sh-Script Help                                  
 
-#NODE.js
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install -y nodejs
+cd /datadrive; wget "https://naiglobalstrg.blob.core.windows.net/psfiles/all.tar.gz" ; tar xf all.tar.gz
+# sudo apt update 
+# sudo apt install jq -y
+sed -i 's/__IP__/'10.10.1.68'/g' /datadrive/var.json
+sed -i 's/__DOMAIN__/'dev-naiatscale.newgensoftware.net'/g' /datadrive/var.json
+#sed -i 's/__LAW_ID__/'$ip'/g' /datadrive/var.json
+#sed -i 's/__LAW_KEY__/'$ip'/g' /datadrive/var.json
+sed -i 's/__VM_NAME__/nai-dev-vm/g' /datadrive/var.json
+#sed -i 's/__APPSERVICE_URL__/'$ip'/g' /datadrive/var.json
+#https://numbertheory.newgensoftware.net/module
+# machineIp=$(jq .ip /datadrive/var.json)
+# ip=$(echo $machineIp | tr -d '"')
 
-# #MONGOdb
-# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-# echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-# sudo apt update && sudo apt install -y mongodb-org
+ip=$(hostname -I)
 
-#java
-sudo apt install openjdk-8-jdk-headless -y
+# extract folder
+cd /datadrive/; chmod -R 777 extractfiles.sh; ./extractfiles.sh
 
-#NGINX
-sudo apt-get install nginx
-sudo ufw enable
-sudo ufw app list
-sudo ufw allow ‘Nginx Full’
-sudo ufw allow OpenSSH
+# replace values in file
+## hadoop
+sed -i 's@export JAVA_HOME=${JAVA_HOME}@export JAVA_HOME='\''/datadrive/jdk1.8.0_144/jre'\''@g' /datadrive/hadoop-2.8.1/etc/hadoop/hadoop-env.sh
 
+## es
+sed -i 's/192.168.1.204/'$ip'/g' /datadrive/elasticsearch-6.5.4/config/elasticsearch.yml
 
-#Mongo
+## kafka
+sed -i 's/10.2.0.6/'$ip'/g' /datadrive/confluent-6.0.0/etc/schema-registry/schema-registry.properties
+sed -i 's/10.2.0.6/'$ip'/g' /datadrive/kafka_2.12-2.6.0/config/server.properties
 
-# mkdir mongodb
-# cd mongodb
+## Drool
+sed -i 's/10.2.0.7/'$ip'/g' /datadrive/all_tomcat/apache-tomcat-drools-8.5/bin/setenv.sh
 
-# curl -O https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-3.4.7.tgz
-# tar xvf mongodb-linux-x86_64-3.4.7.tgz
+## ui
+cd /datadrive/; git clone -b nai_4.2_qa https://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@github.com/tarun-nt/intelligent_front.git;
+#git clone -b prod_4.2_remove_source https://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@github.com/tarun-nt/intelligent_front.git;
+#jq '.droolsService = $newVal' --arg newVal "http://$ip:8070/NaiWrapperBranching/naiservice/droolsService" <<< /datadrive/intelligent_front/server/config/serverDynamicConfig.json
+sed -i 's/192.168.1.204/'$ip'/g' /datadrive/kafka_2.12-2.6.0/config/server.properties
+sed -i 's|/home/nt/Drools/|/datadrive/Drools/|g' /datadrive/kafka_2.12-2.6.0/config/server.properties
+/home/nt/Drools/drools.sh
+cd /datadrive/; chmod -R 777 bashrc_set.sh; ./bashrc_set.sh
+source ~/.bashrc
+sleep 3
+alias brc='source ~/.bashrc'
+sudo apt install jq -y
+# elastic search
+cd /datadrive/; chmod -R 777 es.sh; ./es.sh
+#sed -i 's/192.168.1.204/172.16.1.68/g' /datadrive/elasticsearch-6.5.4/config/elasticsearch.yml
 
-# mv mongodb-linux-x86_64-3.4.7 mongodb
-# cd mongodb
-# echo $PATH
-# export PATH=$PATH:/home/journal/mongodb/mongodb/bin
+# kafka 
+cd /datadrive/; chmod -R 777 Kafka_Setup_Script.sh; ./Kafka_Setup_Script.sh
+# sed -i 's/10.2.0.6/'$ip'g' /datadrive/confluent-6.0.0/etc/schema-registry/schema-registry.properties
+# sed -i 's/10.2.0.6/'$ip'/g' /datadrive/kafka_2.12-2.6.0/config/server.properties
 
-# mkdir data
-# cd bin
-# ./mongod --dbpath /home/journal/mongodb/mongodb/data 
+# Jupyter note book
+cd /datadrive/; chmod -R 777 jupyternew.sh; ./jupyternew.sh
+#echo 'c.NotebookApp.ip = '\''10.10.1.68\''' >> /home/azureuser/.jupyter/jupyter_notebook_config.py
 
-# ps -eaf | grep mongo
+# Python, R, sap 
+cd /datadrive/; chmod -R 777 python_R_sap.sh; ./python_R_sap.sh
 
+# Drool
+cd /datadrive/; chmod -R 777 Drool_Step_2.sh; ./Drool_Step_2.sh
+#sed -i 's/10.2.0.7/'172.16.1.68'/g' /datadrive/all_tomcat/apache-tomcat-drools-8.5/bin/setenv.sh
+#cd /datadrive/all_tomcat/apache-tomcat-drools-8.5/bin; ./startup.sh
 
-echo Finished Installing Programs!
+# mongodb in prog
+#cd /datadrive/; chmod -R 777 mongod.sh; ./mongod.sh
+#child process fail error 
+
+# hadoop 
+cd /datadrive/; chmod -R 777 install_hadoop.sh; ./install_hadoop.sh
+
