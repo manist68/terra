e35a1222df267233622cc1d63c4a3a90a9941036
manist68 <120625434+manist68@users.noreply.github.com>
2023-03-02 14:22:15 +0530

    Update mountdisk.sh

--- vm/mountdisk.sh
+++ vm/mountdisk.sh
@@ -16,35 +16,69 @@ File Edit Options Buffers Tools Sh-Script Help
 
 cd /datadrive; wget "https://naiglobalstrg.blob.core.windows.net/psfiles/all.tar.gz" ; tar xf all.tar.gz
 
+#pvt ip
 ip=$(hostname -I)
 
-# extract folder
-cd /datadrive/; chmod -R 777 extractfiles.sh; ./extractfiles.sh
+cd /datadrive/; tar xf node-v12.18.2-linux-x64.tar.gz; 
+tar xf jdk1.8.0_144.tar.gz; tar xf hadoop-2.8.1.tar.xz; 
+tar xf spark-2.4.5-bin-hadoop2.7.tar.gz; 
+tar xf all_script_files_N.tar.gz; tar xf confluent-6.0.0.tar.gz; 
+tar xf kafka_2.12-2.6.0.tar.gz; tar xf elasticsearch-6.5.4.tar.gz; 
+tar xf Drools.tar.xz;tar xf all_tomcat.tar.xz; tar xf ssl-cert.tar.gz; 
+tar xf mongodb-linux-x86_64-ubuntu1604-4.2.8.tgz;
+
 
 # replace values in file
 ## hadoop
 sed -i 's@export JAVA_HOME=${JAVA_HOME}@export JAVA_HOME='\''/datadrive/jdk1.8.0_144/jre'\''@g' /datadrive/hadoop-2.8.1/etc/hadoop/hadoop-env.sh
 
 ## es
-sed -i 's/192.168.1.204/'$ip'/g' /datadrive/elasticsearch-6.5.4/config/elasticsearch.yml
+sed -i "s|192.168.1.204|${ip}|g" /datadrive/elasticsearch-6.5.4/config/elasticsearch.yml
 
 ## kafka
-sed -i 's/10.2.0.6/'$ip'/g' /datadrive/confluent-6.0.0/etc/schema-registry/schema-registry.properties
-sed -i 's/10.2.0.6/'$ip'/g' /datadrive/kafka_2.12-2.6.0/config/server.properties
+sed -i "s|10.2.0.6|{$ip}|g" /datadrive/confluent-6.0.0/etc/schema-registry/schema-registry.properties
+sed -i "s/10.2.0.6/{$ip}/g" /datadrive/kafka_2.12-2.6.0/config/server.properties
 
 ## Drool
-sed -i 's/10.2.0.7/'$ip'/g' /datadrive/all_tomcat/apache-tomcat-drools-8.5/bin/setenv.sh
+sed -i "s/10.2.0.7/{$ip}/g" /datadrive/all_tomcat/apache-tomcat-drools-8.5/bin/setenv.sh
 
-## ui
-cd /datadrive/; git clone -b nai_4.2_qa https://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@github.com/tarun-nt/intelligent_front.git;
-#git clone -b prod_4.2_remove_source https://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@github.com/tarun-nt/intelligent_front.git;
-#jq '.droolsService = $newVal' --arg newVal "http://$ip:8070/NaiWrapperBranching/naiservice/droolsService" <<< /datadrive/intelligent_front/server/config/serverDynamicConfig.json
-sed -i 's/192.168.1.204/'$ip'/g' /datadrive/kafka_2.12-2.6.0/config/server.properties
-sed -i 's|/home/nt/Drools/|/datadrive/Drools/|g' /datadrive/kafka_2.12-2.6.0/config/server.properties
-/home/nt/Drools/drools.sh
+#setting up 
 
 cd /datadrive/; chmod -R 777 bashrc_set.sh; ./bashrc_set.sh
 
 
-# extract folder
-cd /datadrive/; chmod -R 777 setup_all.sh; ./setup_all.sh
+sudo apt install jq -y
+
+# elastic search
+cd /datadrive/; chmod -R 777 es.sh; ./es.sh
+#sed -i 's/192.168.1.204/172.16.1.68/g' /datadrive/elasticsearch-6.5.4/config/elasticsearch.yml
+
+# kafka
+cd /datadrive/; chmod -R 777 Kafka_Setup_Script.sh; ./Kafka_Setup_Script.sh
+
+# Jupyter note book
+cd /datadrive/; chmod -R 777 jupyternew.sh; ./jupyternew.sh
+#echo 'c.NotebookApp.ip = '\''10.10.1.68\''' >> /home/azureuser/.jupyter/jupyter_notebook_config.py
+
+# Python, R, sap
+cd /datadrive/; chmod -R 777 python_R_sap.sh; ./python_R_sap.sh
+
+# Drool
+cd /datadrive/; chmod -R 777 Drool_Step_2.sh; ./Drool_Step_2.sh
+
+#Tomcat
+cd /datadrive/all_tomcat/apache-tomcat-drools-8.5/bin; ./startup.sh
+
+# mongodb in prog
+sudo apt install curl -y
+
+sudo apt-get install -y mongodb
+
+
+# hadoop
+cd /datadrive/; chmod -R 777 install_hadoop.sh; ./install_hadoop.sh
+
+
+# SAS updata in mongodb and Monitoring of services
+cd /datadrive/; chmod -R 777 monitoringAndSAS.sh; ./monitoringAndSAS.sh
+
