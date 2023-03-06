b6c3419c81aa32e861fc6d1ec43658d2ecc848f7
manist68 <120625434+manist68@users.noreply.github.com>
2023-03-06 11:34:04 +0530

    Update mountdisk.sh

--- vm/mountdisk.sh
+++ vm/mountdisk.sh
@@ -18,16 +18,37 @@ cd /datadrive/; wget "https://sinkstrgadf.blob.core.windows.net/sink/extractfile
 
 cd /datadrive/; chmod -R 777 extractfile_mani.sh; ./extractfile_mani.sh
 sleep 5
-export PS1=${ORIG}${TITLE}
-cd /datadrive/; chmod -R 777 bashrc_set4.sh; ./bashrc_set4.sh
 
+sleep 5
+echo "KAFKA_BROKER=node1:9092"
+echo "KAFKA_BROKER=node1:9092" >> ~/.bashrc
 
+echo "PATH=$PATH:$NODE_HOME/bin"
+echo "PATH=$PATH:$NODE_HOME/bin" >> ~/.bashrc
+
+echo "MONGO_HOME=/datadrive/mongodb-linux-x86_64-ubuntu1604-4.2.8"
+echo "MONGO_HOME=/datadrive/mongodb-linux-x86_64-ubuntu1604-4.2.8" >> ~/.bashrc
+
+echo "PATH=$PATH:$MONGO_HOME/bin"
+echo "PATH=$PATH:$MONGO_HOME/bin" >> ~/.bashrc
+
+echo "JAVA_HOME=/datadrive/jdk1.8.0_144"
+echo "JAVA_HOME=/datadrive/jdk1.8.0_144" >> ~/.bashrc
+
+echo "PATH=$PATH:$JAVA_HOME/bin/"
+echo "PATH=$PATH:$JAVA_HOME/bin/" >> ~/.bashrc
+
+echo "KAFKA_HOME=/datadrive/kafka_2.12-2.6.0"
+echo "KAFKA_HOME=/datadrive/kafka_2.12-2.6.0" >> ~/.bashrc
+
+echo "PATH=$PATH:/datadrive/kafka_2.12-2.6.0/bin"
+echo "PATH=$PATH:/datadrive/kafka_2.12-2.6.0/bin" >> ~/.bashrc
 
 sleep 3
+
 source ~/.bashrc
-sleep 30
-alias brc='source ~/.bashrc'
-sleep 15
+
+sleep 3
 
 cd /datadrive/; chmod -R 777 es.sh; ./es.sh
 cd /datadrive/; chmod -R 777 Kafka_Setup_Script.sh; ./Kafka_Setup_Script.sh
@@ -39,7 +60,5 @@ cd /datadrive/; chmod -R 777 Drool_Step_2.sh; ./Drool_Step_2.sh
 
 cd /datadrive/; chmod -R 777 mongod.sh; ./mongod.sh
 cd /datadrive/; chmod -R 777 install_hadoop.sh; ./install_hadoop.sh
-#cd /datadrive/; git clone -b nai_4.2 https://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@github.com/tarun-nt/intelligent_front.git;
-#cd /datadrive/; chmod -R 777 ui_setup.sh; ./ui_setup.sh
 cd /datadrive/; chmod -R 777 nginx.sh; ./nginx.sh
 cd /datadrive/; chmod -R 777 monitoringAndSAS.sh; ./monitoringAndSAS.sh
